35fd96300ed2846a9a3974962ba2ddf73a0d127e
lqaxx7799 <lqaxx7799@gmail.com>
2020-04-29 13:40:02 +0700

    update script

--- livechat-script.js
+++ livechat-script.js
@@ -1,8 +1,15 @@
-(function(d, s, id, t) {
-    if (d.getElementById(id)) return;
-    var js, fjs = d.getElementsByTagName(s)[0];
-    js = d.createElement(s);
-    js.id = id;
-    js.src = 'https://widget-beta.oncustomer.asia/js/index.js?token=' + t;
-    fjs.parentNode.insertBefore(js, fjs);}
-  (document, 'script', 'oc-chat-widget-bootstrap', '3bafd8ac4537f01d51027d1e30b00eb2'));
+const queryString = window.location.search;
+
+const urlParams = new URLSearchParams(queryString);
+
+const livechat_token = urlParams.get('livechat_token');
+
+(function (d, s, id, t) {
+  if (d.getElementById(id)) return;
+  var js, fjs = d.getElementsByTagName(s)[0];
+  js = d.createElement(s);
+  js.id = id;
+  js.src = 'https://widget-beta.oncustomer.asia/js/index.js?token=' + t;
+  fjs.parentNode.insertBefore(js, fjs);
+}
+  (document, 'script', 'oc-chat-widget-bootstrap', livechat_token));
